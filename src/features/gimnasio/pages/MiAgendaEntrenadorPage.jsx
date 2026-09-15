d022437bdb0a7f4c422c14f08622c2886b8df973
ynavarrete8181 <p1311718181@dn.uleam.edu.ec>
2026-09-14 23:35:35 -0500

    Agrega pagina Mi agenda para entrenador

---
+++ src/features/gimnasio/pages/MiAgendaEntrenadorPage.jsx
@@ -0,0 +1,5 @@
+import { TrabajoEntrenadorPanel } from '../components/TrabajoEntrenadorPanel.jsx';
+
+export function MiAgendaEntrenadorPage() {
+  return <TrabajoEntrenadorPanel tipo="agenda" />;
+}
